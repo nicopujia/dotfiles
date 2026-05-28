7c5d892e8c45ca01bb82bc13388b402d9a892b91
Nicolás Pujia <nicopujia.dev@gmail.com>
2026-05-28 11:21:30 -0700

    always run nvm use

--- misc/shell-config.sh
+++ misc/shell-config.sh
@@ -46,6 +46,7 @@ fi
 
 # load nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use --silent default >/dev/null 2>&1
 
 # load nvm bash_completion
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
